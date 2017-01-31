de101341409aadceff7af1fd87facb7814b1b129
Jonas BELLESSA <jbelless@e1r13p5.42.fr>
2017-01-31 13:21:28 +0100

    create lost mail

--- Code/sources/lostMail.php
+++ Code/sources/lostMail.php
@@ -1,3 +1,17 @@
-<?php
 
-?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+
+	</head>
+	<body>
+		<h1 style="texte-align:center;">Recevoir un nouveau mot de passe par email.</h1>
+		<section >
+			<form method="POST" action="sendMail.php">
+                Courriel: <input type="email" name="mail" required/>
+                <input type ="submit" value="OK" name="submit" />
+			</form>
+		</section>
+	</body>
+</html>
