adc10ac19e317d72a0b93b625296f34c2b6324d7
Jonas BELLESSA <jbelless@e2r9p6.42.fr>
2017-03-17 13:04:04 +0100

    positionnement parfait

--- Code/scripts/add_sprite.js
+++ Code/scripts/add_sprite.js
@@ -49,7 +49,16 @@ function refresh_sprite() {
         if (i == sprite_array.length - 1) {
             img.onload = function(e) {
                 for (var j = 0; j < sprite_array.length; ++j) {
-                    ctx.drawImage(sprite_array[j].img, sprite_array[j].x, sprite_array[j].y, sprite_array[j].width * sprite_array[j].quos, sprite_array[j].height * sprite_array[j].quos);
+                    var posX = sprite_array[j].x - sprite_array[j].width * sprite_array[j].quos / 2;
+                    var posY = sprite_array[j].y - sprite_array[j].height * sprite_array[j].quos / 2;
+                    ctx.save();
+                    ctx.translate(posX, posY)
+                    ctx.rotate(sprite_array[j].ang / 180 * Math.PI);
+                    ctx.drawImage(
+                        sprite_array[j].img, -sprite_array[j].width * sprite_array[j].quos / 2, -sprite_array[j].height * sprite_array[j].quos / 2,
+                        sprite_array[j].width * sprite_array[j].quos,
+                        sprite_array[j].height * sprite_array[j].quos);
+                    ctx.restore();
                 }
             };
         }
@@ -62,20 +71,34 @@ function add_sprite(spr_obj, quos) {
 
     console.log(sp);
 
-    var sprite = new Sprite_icone(0, 0, quos, 0, sp)
+    var tmp;
+
+    var sprite = new Sprite_icone(100, 100, quos, 0, sp)
 
     //Creation de la premiere ligne
 
     var up = document.createElement("button");
+    up.onmousedown = function() { tmp = setInterval(function() { sprite.move(0, -5) }, 200) };
+    up.onclick = function() { sprite.move(0, -5) };
+    up.onmouseup = function() { clearInterval(tmp) };
     up.appendChild(document.createTextNode("up"));
 
     var rollL = document.createElement("button");
+    rollL.onmousedown = function() { tmp = setInterval(function() { sprite.rotate(10) }, 200) };
+    rollL.onclick = function() { sprite.rotate(10) };
+    rollL.onmouseup = function() { clearInterval(tmp) };
     rollL.appendChild(document.createTextNode("roll left"));
 
     var rollR = document.createElement("button");
+    rollR.onmousedown = function() { tmp = setInterval(function() { sprite.rotate(-10) }, 200) };
+    rollR.onclick = function() { sprite.rotate(-10) };
+    rollR.onmouseup = function() { clearInterval(tmp) };
     rollR.appendChild(document.createTextNode("roll right"));
 
     var plus = document.createElement("button");
+    plus.onmousedown = function() { tmp = setInterval(function() { sprite.resize(0.01) }, 200) };
+    plus.onclick = function() { sprite.resize(0.01) };
+    plus.onmouseup = function() { clearInterval(tmp) };
     plus.appendChild(document.createTextNode("+"));
 
     var lign_1 = document.createElement("div");
@@ -87,15 +110,27 @@ function add_sprite(spr_obj, quos) {
     //Creation de la deuxieme ligne
 
     var left = document.createElement("button");
+    left.onmousedown = function() { tmp = setInterval(function() { sprite.move(-5, 0) }, 200) };
+    left.onclick = function() { sprite.move(-5, 0) };
+    left.onmouseup = function() { clearInterval(tmp) };
     left.appendChild(document.createTextNode("left"));
 
     var down = document.createElement("button");
+    down.onmousedown = function() { tmp = setInterval(function() { sprite.move(0, 5) }, 200) };
+    down.onclick = function() { sprite.move(0, 5) };
+    down.onmouseup = function() { clearInterval(tmp) };
     down.appendChild(document.createTextNode("down"));
 
     var right = document.createElement("button");
+    right.onmousedown = function() { tmp = setInterval(function() { sprite.move(5, 0) }, 200) };
+    right.onclick = function() { sprite.move(5, 0) };
+    right.onmouseup = function() { clearInterval(tmp) };
     right.appendChild(document.createTextNode("right"));
 
     var minus = document.createElement("button");
+    minus.onmousedown = function() { tmp = setInterval(function() { sprite.resize(-0.01) }, 200) };
+    minus.onclick = function() { sprite.resize(-0.01) };
+    minus.onmouseup = function() { clearInterval(tmp) };
     minus.appendChild(document.createTextNode("-"));
 
     var lign_2 = document.createElement("div");
